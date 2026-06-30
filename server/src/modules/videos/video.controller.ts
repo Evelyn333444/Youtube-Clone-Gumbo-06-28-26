8f15004c53cd82a1478c5b570a859e1478e6633c
Evelyn333444 <brackesb12@gmail.com>
2026-06-30 09:39:27 -0500

    first push

--- server/src/modules/videos/video.controller.ts
+++ server/src/modules/videos/video.controller.ts
@@ -1,8 +1,16 @@
 import { Request, Response } from "express";
 import busboy from "busboy";
+import { Video } from "./video.model";
+import { StatusCodes } from "http-status-codes";
 import { createVideo } from "./video.service";
 
-const MIME_TYPES = ['']
+const MIME_TYPES = ['video/mp4']
+
+function getPath({videoId, extension}: {
+    videoId: Video['videoId'], extension: Video['extension']
+}) {
+    return `${process.cwd()}/videos/${videoId}.${extension}`;
+}
 
 export async function uploadVideoHandler(req: Request, res: Response){
 
@@ -16,7 +24,37 @@ export async function uploadVideoHandler(req: Request, res: Response){
 
     bb.on('file', async (_, file, info) =>{
 
-        if(!)
+        if(!MIME_TYPES.includes(info.mimeType)){
+            return res.statys(StatusCodes.BAD_REQUEST).send("invalid file type");
+        }
 
-    })
+        const extension = info.mimeType.split('/')[1];
+
+        const filePath = getPath({
+            videoId: video.videoId, 
+            extension, 
+        });
+
+        video.extension = extension;
+
+        await video.save();
+
+        const stream = fs.createWriteStream(filePath)
+
+        file.pipe(stream)
+
+    });
+
+    bb.on('close', () =>{
+        res.writeHead(StatusCodes.CREATED, {
+            Connection: "close",
+            "Content-Type": "application/json",
+        });
+
+         res.write(JSON.stringify(video));
+        res.end();
+    });
+
+    return req.pipe(bb);
+
 }
